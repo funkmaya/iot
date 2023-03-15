aa65d249b65330e37c9fb551614fc74de948769e
Maya Funken <funkmaya04@gmail.com>
2023-03-15 17:11:36 +0100

    Grundstruktur für Raspberry Pi

---
+++ RasperryPi/application_code_01.py
@@ -0,0 +1,5 @@
+
+from application_code_01 import main 
+
+if __name__ == "__main__":
+    main() 
---
+++ RasperryPi/application_code_01/main.py
@@ -0,0 +1,12 @@
+def main(): 
+    
+     """
+     
+     Zentraler Einstiegspunkt in das Programm. 
+     Erzeugt alle benötigten Objekte und sorgt 
+     dafür dass das Programm im Hintergrund laufen kann. 
+     
+     """
+     
+     print("hello world")
+    
